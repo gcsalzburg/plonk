b739b3e8e43ccf96a756e9b5af67be02810f63b3
gcsalzburg <g.cave@kiska.com>
2019-01-06 19:25:25 +0100

    Create custom imgs class

---
+++ php/class.imgs.php
@@ -0,0 +1,36 @@
+<?php
+
+class imgs{
+
+    protected $img_array = array();
+
+    private $valid_suffix = array('t','n','c','h','k');
+
+    public function add_file($fileinfo){
+
+        $fn = basename($fileinfo->getFilename(),'.'.$fileinfo->getExtension());
+
+        if(substr($fn,-2,1) == "_"){
+
+            $suffix = substr($fn,-1);
+
+            if(in_array($suffix,$this->valid_suffix)){
+                $base = substr($fn,0,strlen($fn)-2);
+                $this->img_array[$base][$suffix] = $fileinfo->getFilename();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function get_thumbs_array(){
+        return $this->img_array;
+    }
+
+    public function debug_array(){
+        echo '<pre>';
+        var_dump($this->img_array);
+        echo '</pre>';
+    }
+
+}
