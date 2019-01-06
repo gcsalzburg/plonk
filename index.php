bc62c7fa30da816f54a355637ba3212ec2e19c7d
gcsalzburg <g.cave@kiska.com>
2019-01-06 18:54:30 +0100

    Remvoe old code

--- index.php
+++ index.php
@@ -17,28 +17,6 @@ if(!file_exists(PATH_TO_ROOT_DIR.'/'.$f.'-src')){
 }
 
 
-
-/*
-// Now fetch the page content
-$page_url = "map_custom/".$_SESSION['map_url']."/custom_body.html";
-if(file_exists($page_url)){
-	ob_start();
-	include $page_url;
-	$body_content = ob_get_contents();
-	ob_end_clean();
-}
-
-// Now fetch the page content
-$page_url = "map_custom/".$_SESSION['map_url']."/custom_head.html";
-if(file_exists($page_url)){
-	ob_start();
-	include $page_url;
-	$head_content = ob_get_contents();
-	ob_end_clean();
-}
-*/
-
-
 include 'php/template.php';
 
 function custom_head(){
