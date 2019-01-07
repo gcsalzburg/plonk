bfac87cf176744861b30f5fd3508c8cc231b25ea
gcsalzburg <g.cave@kiska.com>
2019-01-07 21:01:53 +0100

    Tidy up redundant rendering code

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ if(!file_exists($f.'-src')){
 }
 
 
-
 // Fetch metadata
 $json = file_get_contents($folder_src.'/meta.json');
 $meta = json_decode($json);
@@ -27,7 +26,8 @@ $meta = json_decode($json);
 $meta->description = preg_replace('#((https?|ftp)://(\S*?\.\S*?))([\s)\[\]{},;"\':<]|\.\s|$)#i',
 	"<a href=\"$1\" target=\"_blank\">$3</a>$4", $meta->description);
 
-
+// Print page
+// TODO: Move to mustache template
 ?><!doctype html>
 <html lang="en">
 <head>
@@ -48,30 +48,7 @@ $meta->description = preg_replace('#((https?|ftp)://(\S*?\.\S*?))([\s)\[\]{},;"\
 			echo "<h3>".$meta->description."</h3>";
 		?>
 	</header>
-	<main id="gallery" class="gallery" data-next="0" data-folder="<?php echo $folder; ?>">
-		<?php
-/*
-			$count = 0;
-
-
-			$imgs = new imgs();
-
-			$dir = new DirectoryIterator($folder_src.'/');
-			foreach ($dir as $fileinfo) {
-				if ($fileinfo->isFile()) {
-					$imgs->add_file($fileinfo);
-				}
-			}
-			foreach($imgs->get_thumbs_array() as $k => $img){
-				echo '<a href="/'.$folder_src.'/'.$img['h'].'" class="thumb" data-folder="'.$folder.'" data-filename="'.$k.'">';
-					echo '<img alt="'.$k.'" src="/'.$folder_src.'/'.$img['t'].'"/>';
-				echo '</a>';
-			}
-
-			*/
-
-		?>
-	</main>
+	<main id="gallery" class="gallery" data-next="0" data-folder="<?php echo $folder; ?>"></main>
 
 	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 	<script src="/plonk/assets/js/jquery.justifiedGallery.min.js"></script>
