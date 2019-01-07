7c57a0ef69cc487e0998e2e24a9894cd6a3becce
gcsalzburg <g.cave@kiska.com>
2019-01-07 21:07:42 +0100

    Comment and remove old code

--- plonk/assets/js/plonk.js
+++ plonk/assets/js/plonk.js
@@ -3,6 +3,8 @@
 //
 
 $(function(){
+
+    // Load gallery justifier
     $("#gallery").justifiedGallery({
         rowHeight : 350,
         margins : 3,
@@ -15,27 +17,31 @@ $(function(){
             2048 : '_k'
         }
     }).on('jg.complete',function(){
+
+        // Once gallery loaded, append download links to each item
         $(this).children().each(function(i){
             $(this).append('<a href="/d/'+$(this).attr('data-folder')+'/'+$(this).attr('data-filename')+'" class="download_orig" target="_blank">Download Original</a>');
         });
+
+        // Initialise swipebox
         $('.thumb').swipebox();
 
     });
 
-    $("#gallery").on('mouseenter','.thumb',function(){
-        $(this).addClass('hover');
-    }).on('mouseleave','.thumb',function(){
-        $(this).removeClass('hover');
+    // Prevent clicks on download link propagating up (and opening swipebox gallery as well)
+    $("#gallery").on('click','.download_orig',function(e){
+        e.stopPropagation();
     });
 
+    // Scroll handler for infinite scrolling
     $(window).scroll(function() {
         if($(window).scrollTop() + $(window).height() == $(document).height()) {
             fetch_next();
         }
-      });
+    });
 
-
-      function fetch_next(){
+    // Call function to load next items for infinite scroller
+    function fetch_next(){
         var next = $("#gallery").attr('data-next');
         var folder = $("#gallery").attr('data-folder');
         $.getJSON("/plonk/fetch.php",{
@@ -45,14 +51,14 @@ $(function(){
             if(data.has_imgs){
                 $.each(data.imgs, function(i, item){
                     $("#gallery").append('<a href="'+item.link+'" class="thumb" data-folder="'+item.folder+'" data-filename="'+item.filename+'">' +
-                        '<img alt="'+item.alt+'" src="'+item.src+'">' +
-                        '</a>');
+                    '<img alt="'+item.alt+'" src="'+item.src+'">' +
+                    '</a>');
                 }); 
                 $('#gallery').justifiedGallery('norewind'); 
             }
             $("#gallery").attr('data-next',data.next);
         });
-      }
+    }
 
-      fetch_next();
+    fetch_next();
 });
