c66a1b59fb5edd3be469a5c97bc5d89d50f59bf4
gcsalzburg <g.cave@kiska.com>
2019-01-07 20:35:59 +0100

    Cleanup old jquery reference

--- index.php
+++ index.php
@@ -73,12 +73,7 @@ $meta->description = preg_replace('#((https?|ftp)://(\S*?\.\S*?))([\s)\[\]{},;"\
 		?>
 	</main>
 	
-<!--	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script> -->
-	<script
-  src="https://code.jquery.com/jquery-2.2.4.min.js"
-  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-  crossorigin="anonymous"></script>
-
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 	<script src="/plonk/assets/js/jquery.justifiedGallery.min.js"></script>
 	<script src="/plonk/assets/js/jquery.swipebox.min.js"></script>
 	<script src="/plonk/assets/js/scripts.js"></script>
